443abd23c95db0b12055a204537adfd7e2284f0b
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-03-13 16:35:15 -0500

    Refactoring: Create a component for extract some logic of app and can reuse this component several times in app

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,16 +1,16 @@
 interface SquareProps {
-    children: React.ReactNode
-    position: number
-    isSelect?: boolean
-    handleClick?: (index:number) => void
+	position: number
+	handleClick?: (index: number) => void
+	children: React.ReactNode
 }
-export const Square = ({children,position,isSelect,handleClick}:SquareProps) => {
-	const style = isSelect ? "isSelect" : ""
-	return <div 
-		className={`square ${style}`} 
-		onClick={() => handleClick? handleClick(position): null}
-	>  
-		{children} 
-	</div>
-    
+export const Square = ({ position, handleClick, children }: SquareProps) => {
+	return (
+		<div
+			className="square"
+			role={`square${position}`}
+			onClick={() => (handleClick ? handleClick(position) : null)}
+		>
+			{children}
+		</div>
+	)
 }
