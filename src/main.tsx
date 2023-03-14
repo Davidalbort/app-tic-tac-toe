c73630214610a6f173bd3de35634998d15f95e8e
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-03-14 16:10:20 -0500

    Styles: Add styles in css fo app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom/client"
-import { App } from "./app"
+import { App } from "./App"
+import "./style.css"
 
 ReactDom.createRoot(document.getElementById("app") as HTMLElement).render(
 	<React.StrictMode>
